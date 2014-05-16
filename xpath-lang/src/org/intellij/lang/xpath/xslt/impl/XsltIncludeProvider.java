dfd39deb2f628a9bb6891acdb225424e950c54b5
VISTALL <vistall.valeriy@gmail.com>
2014-05-16 09:07:42 +0300

    compilation fixed

--- xpath-lang/src/org/intellij/lang/xpath/xslt/impl/XsltIncludeProvider.java
+++ xpath-lang/src/org/intellij/lang/xpath/xslt/impl/XsltIncludeProvider.java
@@ -16,67 +16,95 @@
 
 package org.intellij.lang.xpath.xslt.impl;
 
+import java.util.ArrayList;
+
+import org.intellij.lang.xpath.xslt.XsltSupport;
+import org.jetbrains.annotations.NotNull;
 import com.intellij.ide.highlighter.XmlFileType;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.impl.include.FileIncludeInfo;
 import com.intellij.psi.impl.include.FileIncludeProvider;
+import com.intellij.util.Consumer;
 import com.intellij.util.indexing.FileContent;
 import com.intellij.util.text.CharArrayUtil;
 import com.intellij.util.xml.NanoXmlUtil;
-import org.intellij.lang.xpath.xslt.XsltSupport;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
 
 /**
  * @author Dmitry Avdeev
  */
-public class XsltIncludeProvider extends FileIncludeProvider {
-  @NotNull
-  public String getId() {
-    return "xslt";
-  }
+public class XsltIncludeProvider extends FileIncludeProvider
+{
+	@Override
+	@NotNull
+	public String getId()
+	{
+		return "xslt";
+	}
 
-  public boolean acceptFile(VirtualFile file) {
-    return file.getFileType() == XmlFileType.INSTANCE;
-  }
+	@Override
+	public boolean acceptFile(VirtualFile file)
+	{
+		return file.getFileType() == XmlFileType.INSTANCE;
+	}
 
-  @NotNull
-  public FileIncludeInfo[] getIncludeInfos(FileContent content) {
-    CharSequence contentAsText = content.getContentAsText();
-    if (CharArrayUtil.indexOf(contentAsText, XsltSupport.XSLT_NS, 0) == -1) return FileIncludeInfo.EMPTY;
-    final ArrayList<FileIncludeInfo> infos = new ArrayList<FileIncludeInfo>();
-    NanoXmlUtil.IXMLBuilderAdapter builder = new NanoXmlUtil.IXMLBuilderAdapter() {
+	@Override
+	public void registerFileTypesUsedForIndexing(@NotNull Consumer<FileType> fileTypeSink)
+	{
+		fileTypeSink.consume(XmlFileType.INSTANCE);
+	}
 
-      boolean isXslt;
-      boolean isInclude;
-      @Override
-      public void startElement(String name, String nsPrefix, String nsURI, String systemID, int lineNr) throws Exception {
-        boolean isXsltTag = XsltSupport.XSLT_NS.equals(nsURI);
-        if (!isXslt) { // analyzing start tag
-          if (!isXsltTag) {
-            stop();
-          } else {
-            isXslt = true;
-          }
-        }
-        isInclude = isXsltTag && ("include".equals(name) || "import".equals(name));
-      }
+	@Override
+	@NotNull
+	public FileIncludeInfo[] getIncludeInfos(FileContent content)
+	{
+		CharSequence contentAsText = content.getContentAsText();
+		if(CharArrayUtil.indexOf(contentAsText, XsltSupport.XSLT_NS, 0) == -1)
+		{
+			return FileIncludeInfo.EMPTY;
+		}
+		final ArrayList<FileIncludeInfo> infos = new ArrayList<FileIncludeInfo>();
+		NanoXmlUtil.IXMLBuilderAdapter builder = new NanoXmlUtil.IXMLBuilderAdapter()
+		{
 
-      @Override
-      public void addAttribute(String key, String nsPrefix, String nsURI, String value, String type) throws Exception {
-        if (isInclude && "href".equals(key)) {
-          infos.add(new FileIncludeInfo(value));
-        }
-      }
+			boolean isXslt;
+			boolean isInclude;
 
-      @Override
-      public void endElement(String name, String nsPrefix, String nsURI) throws Exception {
-        isInclude = false;
-      }
-    };
+			@Override
+			public void startElement(String name, String nsPrefix, String nsURI, String systemID, int lineNr) throws Exception
+			{
+				boolean isXsltTag = XsltSupport.XSLT_NS.equals(nsURI);
+				if(!isXslt)
+				{ // analyzing start tag
+					if(!isXsltTag)
+					{
+						stop();
+					}
+					else
+					{
+						isXslt = true;
+					}
+				}
+				isInclude = isXsltTag && ("include".equals(name) || "import".equals(name));
+			}
 
-    NanoXmlUtil.parse(CharArrayUtil.readerFromCharSequence(contentAsText), builder);
-    return infos.toArray(new FileIncludeInfo[infos.size()]);
-  }
+			@Override
+			public void addAttribute(String key, String nsPrefix, String nsURI, String value, String type) throws Exception
+			{
+				if(isInclude && "href".equals(key))
+				{
+					infos.add(new FileIncludeInfo(value));
+				}
+			}
+
+			@Override
+			public void endElement(String name, String nsPrefix, String nsURI) throws Exception
+			{
+				isInclude = false;
+			}
+		};
+
+		NanoXmlUtil.parse(CharArrayUtil.readerFromCharSequence(contentAsText), builder);
+		return infos.toArray(new FileIncludeInfo[infos.size()]);
+	}
 }
