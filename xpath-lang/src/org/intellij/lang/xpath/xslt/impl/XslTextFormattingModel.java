8e5fa296bb4c45266aa6064a7de4f0d28c3bd16b
VISTALL <vistall.valeriy@gmail.com>
2015-08-18 22:33:10 +0300

    compilation

--- xpath-lang/src/org/intellij/lang/xpath/xslt/impl/XslTextFormattingModel.java
+++ xpath-lang/src/org/intellij/lang/xpath/xslt/impl/XslTextFormattingModel.java
@@ -83,8 +83,8 @@ class XslTextFormattingModel implements FormattingModel {
         return myModel.replaceWhiteSpace(textRange, whiteSpace);
     }
 
-    public TextRange shiftIndentInsideRange(TextRange range, int indent) {
-        return myModel.shiftIndentInsideRange(range, indent);
+    public TextRange shiftIndentInsideRange(ASTNode node, TextRange range, int indent) {
+        return myModel.shiftIndentInsideRange(node, range, indent);
     }
 
     public void commitChanges() {
